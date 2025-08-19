0b5ca22d9f383034f7cd05fef3ee8d84b3c20d3c
Yifei Gu <yifeigu@yahoo.com>
2025-08-18 18:10:47 -0700

    update for Tamera

--- olympus_segmentation/aere/main_aere_with_multi_labels_model.py
+++ olympus_segmentation/aere/main_aere_with_multi_labels_model.py
@@ -3,15 +3,15 @@ import torch
 import segmentation_models_pytorch as smp
 from torch.utils.data import DataLoader
 from aere_dataset import MultiLabelSegDataset
-from olympus_segmentation.endo.transforms import get_val_transforms
-from olympus_segmentation.aere.visualizing_aere_with_multi_labels_model import visualize_all_predictions_without_manual_annotation
+from transforms import get_val_transforms
+from visualizing_aere_with_multi_labels_model import visualize_all_predictions_without_manual_annotation
 from torchvision import models
 
 # --------------------- Main ---------------------
 if __name__ == '__main__':
     # Set your directories.
     channels = ['DAPI', 'FITC', 'TRITC']
-    model_path =os.path.join(os.path.dirname(__file__), "weights", 'aere_multilabels_model_train_on_tamera.pth') # or 'aere_multilabels_model_train_on_stefan.pth'
+    model_path =r'C:\Users\Yifei\Documents\roots\olympus_segmentation\weights\aere_multilabels_model_train_on_stefan.pth' # or 'aere_multilabels_model_train_on_stefan.pth'
     data_folder = r'C:\Users\Yifei\Documents\new_aere_model_training_on_tamera\val'         # Contains subfolders: image, annotation. Image folder contains another layer of subfolders for each sample. Each sample folder contains 3-channel images.
     output_folder = r'C:\Users\Yifei\Documents\new_aere_model_training_on_tamera\results'          # Directory to save the prediction results.
     if not os.path.exists(output_folder):
